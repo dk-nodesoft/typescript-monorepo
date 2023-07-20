d875806d3b9bc639777fcfaeeb478f8d2d5d2f7f
Niels P. Sorensen <sorensen.niels@gmail.com>
2023-07-20 23:00:20 +0200

    Added nestjs core

---
+++ packages/nestjs-core/jest.config.cjs
@@ -0,0 +1,27 @@
+const { getTsConfigBasePaths, sanitizePackageName } = require('@dk-nodesoft/jest-config');
+const { resolve } = require('path');
+const base = require('@dk-nodesoft/jest-config/jest-nest');
+
+const { name } = require('./package.json');
+
+const tsConfigFile = resolve('./tsconfig.json');
+const tsConfigBasePaths = getTsConfigBasePaths(tsConfigFile);
+
+process.env.JEST_JUNIT_OUTPUT_NAME = `report-${sanitizePackageName(name)}.xml`;
+process.env.JEST_SUITE_NAME = name;
+
+module.exports = {
+  ...base,
+  rootDir: './src',
+
+  displayName: `${name}:unit`,
+  moduleNameMapper: {
+    ...tsConfigBasePaths
+  },
+
+  testTimeout: 240000,
+  globalSetup: '<rootDir>/specs/globalSetup.ts',
+  globalTeardown: '<rootDir>/specs/globalTeardown.ts',
+  maxConcurrency: 1,
+  maxWorkers: 1
+};
